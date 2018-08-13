5649b11505bc83e5913ce7ceb45e9cbc4734c19e
Yunra <mekstrom86@gmail.com>
2018-08-13 19:23:56 +0200

    Added all standard legal sets

--- mybot.js
+++ mybot.js
@@ -41,6 +41,82 @@ function setActivity(booster) {
     var randcard = Math.floor(Math.random() * booster.length);
     client.user.setActivity(booster[randcard].toString(), { type: 'PLAYING'});
 }
+
+//Takes a set (3 letters) and an amount of cards in the booster, default 14
+function generateBoosterFromScryfall(message, set, amount = 14, setname = set) {
+    message.channel.send("Hold on, generating booster from " + setname);
+    var mythic = [];
+    var rare = [];
+    var uncommon = [];
+    var common = [];
+    request('https://api.scryfall.com/cards/search?q=e%3A' + set + '+is%3Abooster+-t%3Abasic', {json: true}, function (error, response, body) {
+        var set = JSON.parse(JSON.stringify(body));
+        var next_page = "";
+        let cards = set.data;
+        if (set.has_more = "true") {
+            next_page = set.next_page.replace("\u0026", "");
+            request(next_page, {json: true}, function (error, response, body2) {
+                var moreinset = JSON.parse(JSON.stringify(body2));
+                cards = cards.concat(moreinset.data);
+                for (card of cards) {
+                    if (card.rarity == "common") {
+                        common.push(card);
+                    } else if(card.rarity == "uncommon") {
+                        uncommon.push(card);
+                    } else if (card.rarity == "rare") {
+                        rare.push(card);
+                    } else if (card.rarity == "mythic") {
+                        mythic.push(card);
+                    }
+                }
+                common = shuffleArray(common);
+                uncommon = shuffleArray(uncommon);
+                rare = shuffleArray(rare);
+                mythic = shuffleArray(mythic);
+                var booster = common.slice(0,10);
+                booster = booster.concat(uncommon.slice(0,3));
+                if (Math.floor(Math.random() * 7) == 0) {
+                    booster = booster.concat(mythic.slice(0,1));
+                } else {
+                    booster = booster.concat(rare.slice(0,1));
+                }
+                var cardnames = [];
+                for (card of booster) {
+                    cardnames.push(card.name);
+                }
+                message.channel.send(new Discord.RichEmbed().setDescription(cardnames).setTitle(amount + " cards from " + setname).setURL(createScryfallLink(cardnames, "rarity", "dom")));
+            });
+        } else {
+            for (card of legalCards) {
+                if (card.rarity == "common") {
+                    common.push(card);
+                } else if(card.rarity == "uncommon") {
+                    uncommon.push(card);
+                } else if (card.rarity == "rare") {
+                    rare.push(card);
+                } else if (card.rarity == "mythic") {
+                    mythic.push(card);
+                }
+            }
+            common = shuffleArray(common);
+                uncommon = shuffleArray(uncommon);
+                rare = shuffleArray(rare);
+                mythic = shuffleArray(mythic);
+                var booster = common.slice(0,10);
+                booster = booster.concat(uncommon.slice(0,3));
+                if (Math.floor(Math.random() * 7) == 0) {
+                    booster = booster.concat(mythic.slice(0,1));
+                } else {
+                    booster = booster.concat(rare.slice(0,1));
+                }
+                var cardnames = [];
+                for (card of booster) {
+                    cardnames.push(card.name);
+                }
+                message.channel.send(new Discord.RichEmbed().setDescription(cardnames).setTitle(amount + " cards from " + setname).setURL(createScryfallLink(cardnames, "rarity", "dom")));
+        }
+    });
+}
             
 client.on("message", (message) => {
     if (message.content.startsWith("!p1p1 paupercube")) {
@@ -56,91 +132,25 @@ client.on("message", (message) => {
         });
     }
     else if (message.content.startsWith("!p1p1 dom")) {
-        var set = "dom";
-        var highest_collector_number = 269;
-        var mythic = [];
-        var rare = [];
-        var uncommon = [];
-        var common = [];
-        request('https://api.scryfall.com/cards/search?q=e%3A' + set + '+is%3Abooster+-t%3Abasic', {json: true}, function (error, response, body) {
-            var set = JSON.parse(JSON.stringify(body));
-            var next_page = "";
-            let cards = set.data;
-            if (set.has_more = "true") {
-                next_page = set.next_page.replace("\u0026", "");
-                request(next_page, {json: true}, function (error, response, body2) {
-                    var moreinset = JSON.parse(JSON.stringify(body2));
-                    cards = cards.concat(moreinset.data);
-                    let legalCards = [];
-                    for(card of cards) {
-                        if (parseInt(card.collector_number) <= highest_collector_number) {
-                            legalCards.push(card);
-                        }
-                    };
-                    for (card of legalCards) {
-                        if (card.rarity == "common") {
-                            common.push(card);
-                        } else if(card.rarity == "uncommon") {
-                            uncommon.push(card);
-                        } else if (card.rarity == "rare") {
-                            rare.push(card);
-                        } else if (card.rarity == "mythic") {
-                            mythic.push(card);
-                        }
-                    }
-                    common = shuffleArray(common);
-                    uncommon = shuffleArray(uncommon);
-                    rare = shuffleArray(rare);
-                    mythic = shuffleArray(mythic);
-                    var booster = common.slice(0,10);
-                    booster = booster.concat(uncommon.slice(0,3));
-                    if (Math.floor(Math.random() * 7) == 0) {
-                        booster = booster.concat(mythic.slice(0,1));
-                    } else {
-                        booster = booster.concat(rare.slice(0,1));
-                    }
-                    var cardnames = [];
-                    for (card of booster) {
-                        cardnames.push(card.name);
-                    }
-                    message.channel.send(new Discord.RichEmbed().setDescription(cardnames).setTitle("14 cards from Dominaria").setURL(createScryfallLink(cardnames, "rarity", "dom")));
-                });
-            } else {
-                let legalCards = [];
-                for(card of cards) {
-                    if (parseInt(card.collector_number) <= highest_collector_number) {
-                        legalCards.push(card);
-                    }
-                };
-                for (card of legalCards) {
-                    if (card.rarity == "common") {
-                        common.push(card);
-                    } else if(card.rarity == "uncommon") {
-                        uncommon.push(card);
-                    } else if (card.rarity == "rare") {
-                        rare.push(card);
-                    } else if (card.rarity == "mythic") {
-                        mythic.push(card);
-                    }
-                }
-                common = shuffleArray(common);
-                    uncommon = shuffleArray(uncommon);
-                    rare = shuffleArray(rare);
-                    mythic = shuffleArray(mythic);
-                    var booster = common.slice(0,10);
-                    booster = booster.concat(uncommon.slice(0,3));
-                    if (Math.floor(Math.random() * 7) == 0) {
-                        booster = booster.concat(mythic.slice(0,1));
-                    } else {
-                        booster = booster.concat(rare.slice(0,1));
-                    }
-                    var cardnames = [];
-                    for (card of booster) {
-                        cardnames.push(card.name);
-                    }
-                    message.channel.send(new Discord.RichEmbed().setDescription(cardnames).setTitle("14 cards from Dominaria").setURL(createScryfallLink(cardnames, "rarity", "dom")));
-            }
-        });
+        generateBoosterFromScryfall(message, "dom", 14, "Dominaria");
+    }
+    else if (message.content.startsWith("!p1p1 rix")) {
+        generateBoosterFromScryfall(message, "rix", 14, "Rivals of Ixalan");
+    }
+    else if (message.content.startsWith("!p1p1 xln")) {
+        generateBoosterFromScryfall(message, "xln", 14, "Ixalan");
+    }
+    else if (message.content.startsWith("!p1p1 hou")) {
+        generateBoosterFromScryfall(message, "hou", 14, "Hour of Devastation");
+    }
+    else if (message.content.startsWith("!p1p1 akh")) {
+        generateBoosterFromScryfall(message, "akh", 14, "Amonkhet");
+    }
+    else if (message.content.startsWith("!p1p1 aer")) {
+        generateBoosterFromScryfall(message, "aer", 14, "Aether Revolt");
+    }
+    else if (message.content.startsWith("!p1p1 kld")) {
+        generateBoosterFromScryfall(message, "kld", 14, "Kaladesh");
     }
     else if (message.content.startsWith("!p1p1 m19")) {
         //Create the booster for this set
@@ -164,7 +174,14 @@ client.on("message", (message) => {
     }
     else if (message.content.startsWith("!p1p1 help")) {
         message.channel.send("This bot supports the following commands \n \n \
-!p1p1 m19 - Generate a 15 card booster pack for Core Set 2019. \n \
+!p1p1 m19 - Generate a 14 card booster pack for Core Set 2019. \n \
+!p1p1 dom - Generate a 14 card booster pack for Dominaria. \n \
+!p1p1 rix - Generate a 14 card booster pack for Rivals of Ixalan. \n \
+!p1p1 xln - Generate a 14 card booster pack for Ixalan. \n \
+!p1p1 hou - Generate a 14 card booster pack for Hour of Devastation. \n \
+!p1p1 akh - Generate a 14 card booster pack for Amonkhet. \n \
+!p1p1 aer - Generate a 14 card booster pack for Aether Revolt. \n \
+!p1p1 kld - Generate a 14 card booster pack for Kaladesh. \n \
 !p1p1 paupercube - Generate a 15 card booster pack for thepaupercube.com \n \
 !p1p1 brewchallenge - You get 1 randomly picked card and have to build a deck around it. \n \
 \n \
