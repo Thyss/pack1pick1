4c57f31b6ab51e1db7edaee030326a6c77bc8a12
omniczech <mikepetschek@gmail.com>
2018-08-13 15:11:45 -0400

    Attempted to add ct support
    Currently CT's download form uses the most recently viewed cube based on
    cookie. Going to attempt to just do a request to cube page to get raw
    html and remove all non card objects

--- mybot.js
+++ mybot.js
@@ -23,12 +23,21 @@ function getCardsFromFile(file, amount) {
     return cards;
 }
 
+//Get a booster with a certain number of cards from a cubetutor
+function getCardsFromCT(response, amount) {
+    var selected = [];
+    selected = response.toString().split('\n');
+    var shuffled = shuffleArray(selected);
+    var cards = shuffled.slice(0,amount);
+    return cards;
+}
+
 //Create the scryfall link so you can view the cards easily
 function createScryfallLink(cardlist, order = "rarity", set = "m19") {
     var scryfalllink = "https://scryfall.com/search?unique=cards&as=grid&order=" + order + "&set=" + set + "&q=!";
     scryfalllink += cardlist.join('+or+!');
     scryfalllink = scryfalllink.replace(/ /g, '-');
-    scryfalllink = scryfalllink.replace(/\s/g, ''); 
+    scryfalllink = scryfalllink.replace(/\s/g, '');
     return scryfalllink;
 }
 
@@ -37,12 +46,12 @@ function setActivity(booster) {
     var randcard = Math.floor(Math.random() * booster.length);
     client.user.setActivity(booster[randcard].toString(), { type: 'PLAYING'});
 }
-            
+
 client.on("message", (message) => {
     if (message.content.startsWith("!p1p1 paupercube")) {
         let booster = getCardsFromFile('./cardsets/paupercube.txt', 15);
             //Create scryfall link for images
-        var scryfalllink = createScryfallLink(booster, "name");           
+        var scryfalllink = createScryfallLink(booster, "name");
         setActivity(booster);
         message.channel.send(new Discord.RichEmbed().setDescription(booster).setURL(scryfalllink).setTitle("15 cards from Thepaupercube.com"));
     }
@@ -67,6 +76,36 @@ client.on("message", (message) => {
         setActivity(booster);
         message.channel.send(new Discord.RichEmbed().setDescription(booster).setTitle("15 cards from Core Set 2019").setURL(createScryfallLink(booster, "rarity", "m19")));
     }
+    else if (message.content.startsWith("!p1p1 ct")) {
+        let ctID = message.content.replace("!p1p1 ct ", "")
+        console.log(ctID);
+        if (/^[0-9]*$/.test(ctID)){
+          let options = {
+            url:'http://www.cubetutor.com/viewcube.exportform.exportlistform', method:'POST',
+            form: {"t:ac": ctID,
+                  "t:formdata": "L+PVfUyWS7KfKAY1b/cATEoMx00=:H4sIAAAAAAAAAFvzloG1XI5BJiwztdy5NCnVKrWiIL+oJC2/KFcvLTMntaSyILW4iME0vyhdL7EgMTkjVa8kEShUUlRpqpecX5Sak5mkl5RYnKrnmAQUTEwucctMzUlRCU4tKS1QDT3M/VD0+B8mBkYfBu7k/LySovwcv8Tc1BIGIZ+sxLJE/ZzEvHT94JKizLx064qCEgYOkJ0hQDuJcJMjqW4KKMpPTi0uDi5Nys0sLs7Mzzu8LsUk7du8c0wMDBUF5TIMUthsLAYpLwHa54DXvuT83IL8vNS8kmI9sAUlmNbNDP4kuXVLizMTA5MPA0dyTiZQtWdKIUMdOHhSc1JzgQKg4AELgYMDYnm8EYJpAADQ2n82sgEAAA==",
+                  fileType:'CUBE_TXT',
+                  submit_0: "Export",
+                  "t:submit:": '["submit_2","submit_0"]'
+                },
+                headers: {
+                  'cookie': 'JSESSIONID=46160491092C9D57C43C94566C28C368;'
+                }
+          }
+          request(options,
+                   function (error, response, body) {
+                      let booster = getCardsFromCT(body, 15);
+                      console.log('body:', body); // Print the HTML for the Google homepage.
+                      console.log("request", response.req['_header']);
+                      console.log(booster);
+                      var scryfalllink = createScryfallLink(booster, "name");
+                      setActivity(booster);
+                      message.channel.send(new Discord.RichEmbed().setDescription(booster).setURL(scryfalllink).setTitle("Results from cube with id: " + ctID));
+          });
+        } else {
+          message.channel.send(new Discord.RichEmbed().setDescription("The ID you entered is invalid").setTitle("Error"));
+        }
+    }
     else if (message.content.startsWith("!p1p1 about")) {
         message.channel.send("\
             This bot was made to generate booster packs and discuss what to pick first in packs. More sets will be available as i add them, feel free to come with feedback on what sets you would like to see supported. \n \n Author: Martin Ekström \n Discord username: Yunra \n Support development by donating: https://www.paypal.me/yunra");
@@ -88,4 +127,4 @@ If you can not see the boosters, check your discord settings if you have disable
     }
 });
 
-client.login(process.env.discord_token);
+client.login("NDc4NjAzMTM4MjEyMDM2NjA5.DlNGYg.eTAdpyDVJDs0g3gTztnvJqSy5Mo");
